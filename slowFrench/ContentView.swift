564b9b44b144082bdf57f1e8448f85c5c9085827
AndreaMonroy <andrearamonroy@gmail.com>
2022-06-24 17:12:36 -0400

    Initial Commit

---
+++ slowFrench/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  slowFrench
+//
+//  Created by Andrea on 6/24/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ slowFrench/slowFrenchApp.swift
@@ -0,0 +1,17 @@
+//
+//  slowFrenchApp.swift
+//  slowFrench
+//
+//  Created by Andrea on 6/24/22.
+//
+
+import SwiftUI
+
+@main
+struct slowFrenchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
